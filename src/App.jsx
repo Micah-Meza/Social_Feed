bd7abd8647c2cfe8df930fa71c27a518713fa4aa
Micah <airrenno@yahoo.com>
2023-02-05 15:08:15 -0600

    Created the Social Feed React app along with the framework.

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+
+
+
+function App() {
+
+  const [names, setName] = useState([{nameEntry : "Micah Meza"}])
+  const [postEntry, setPostEntry] = useState([{message : "I love my kids."}])
+
+  return (
+    <div>
+        <table>
+
+      </table>
+    </div>
+  );
+}
+
+export default App;
